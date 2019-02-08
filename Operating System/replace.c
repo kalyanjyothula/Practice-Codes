bc43329cc5310fd82c916e32ea895973f7dfc26d
KalyanJyothula <38788245+kalyanjyothula@users.noreply.github.com>
2019-02-08 20:06:27 +0530

    Update replace.c

--- Operating System/replace.c
+++ Operating System/replace.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 #include<sys/file.h>
 #include<sys/dir.h>
 #include<dirent.h>
@@ -13,14 +14,16 @@ int main()
   struct dirent *lx=NULL;
   while((lx=readdir(fp))!=NULL)
   {     
-    char *o=rindex(lx->d_name,'/');
+    if(!strcmp(lx->d_name,".") || !strcmp(lx->d_name,"..")) continue;
+    char *o=rindex(lx->d_name,'/');    // symbol u want to remove {ex . or \} from filename
     if(o!=NULL)
-    {  
+    {  p=1;
        int l=strlen(lx->d_name) - strlen(o);
-       char fill[1900]; 
+       char *fill;        
        strncpy(fill,lx->d_name,l);
-       printf("%s %s\n",fill,lx->d_name);
-       //rename(lx->d_name,fill);  
+       //printf("%s  -- %s\n",fill,lx->d_name);
+       rename(lx->d_name,fill);  
+       //*fill='\0';
     }  
   }
   if(p==0) printf("-1\n");
